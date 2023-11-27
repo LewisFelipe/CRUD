4739095b4138902e9b2335482bf846df950c5915
LewisFelipe <aluisiusfeliposu@gmail.com>
2023-11-27 17:32:49 -0300

    Rodrigao no projeto

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include("./layout/menu.php");
 <section>
     <h1 class="display-3 text-white d-table mt-1 mb-3 ml-auto mr-auto"><strong>Bem vindo!!</strong></h1>
     <div class="rounded-container center text-center p-4" style="min-height: 100px">
-        <h3>Este é um projeto para a matéria de Laboratório III feito por Henrique Carneiro e Luis Felipe Paludetto Silva.</h3>
+        <h3>Este é um projeto para a matéria de Laboratório III feito por Henrique Carneiro, Rodrigo Moratto Aguilhar e Luis Felipe Paludetto Silva.</h3>
     </div>
 
 </section>
